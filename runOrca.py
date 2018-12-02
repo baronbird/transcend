3ed6c25463ee12a1640d33fab8517394dadedfe2
Joseph Kimlinger <jkimling@crcfe01.crc.nd.edu>
2018-12-02 14:00:01 -0500

    generated 4 node graphlet counts

--- runOrca.py
+++ runOrca.py
@@ -1,20 +1,6 @@
 '''
-This script runs orca on every file in a specific subdirectory structure as follows:
-	main_dir
-	|
-	|------ subdirectory
-	|	|
-	|	|----- networks
-	|	|
-	|------ another subdirectory
-	|	|
-	|	|----- more networks
-	.
-	.
-	.
-
-The resulting graphlt counts will be placed in the same structure with the same subdirectory names, but with the name of main_dir
-specified by command line argument:
+This script runs orca on every file in a directory, searching subdirectories recursively for .txt files.
+It will generate the ouput in the same directory structure as the input dir except that the root directory will have the name specified by the OUPUT_DIR command line argument
 
 python runOrca.py main_dir output_dir
 '''
@@ -27,8 +13,10 @@ if len(sys.argv) != 3:
 	print "Usage: {} INPUT_DIR OUTPUT_DIR".format(sys.argv[0])
 	sys.exit(1)
 
-for f in glob.glob('{}/*/*.txt'.format(sys.argv[1])):
-	out = f.replace(sys.argv[1], sys.argv[2])
-	if not os.path.exists(os.path.dirname(out)):
-		os.makedirs(os.path.dirname(out))
-	os.system('./orca 5 {} {}'.format(f, out))
+for root, dirname, filename in os.walk(sys.argv[1]):
+	out = root.replace(sys.argv[1], sys.argv[2]) + "/"
+	print root
+	if not os.path.exists(out):
+		os.makedirs(out)
+	for f in filename:
+		os.system('./orca 4 {} {}'.format(root + "/" + f, out + f))
